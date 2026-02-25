0c0d3ec08616465438ee48b8fa36a90c2021778a
pppg9dptzv-wq <pppg9dptzv@privaterelay.appleid.com>
2026-02-25 17:38:50 +0100

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,8 +0,0 @@
-window.CALI_SUPABASE_CONFIG = {
-  url: "https://tegwtfkazosetiumsbme.supabase.co",
-  anonKey: "sb_publishable_PSf8SMlvd1WIlDrkWT8znw_ibLVgH5a",
-  table: "athletes",
-  storageBucket: "athletes-media",
-  confirmationFunction: "send-submission-confirmation",
-  authRedirectTo: "https://pppg9dptzv-wq.github.io/v3/cc.html",
-};
