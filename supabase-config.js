60fa60622a1911e88f56afe8f6865a2a8b4eaf35
pppg9dptzv-wq <pppg9dptzv@privaterelay.appleid.com>
2026-02-25 16:22:06 +0100

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -2,4 +2,6 @@ window.CALI_SUPABASE_CONFIG = {
   url: "https://tegwtfkazosetiumsbme.supabase.co",
   anonKey: "sb_publishable_PSf8SMlvd1WIlDrkWT8znw_ibLVgH5a",
   table: "athletes",
+  storageBucket: "athletes-media",
+  emailRedirectTo: "",
 };
